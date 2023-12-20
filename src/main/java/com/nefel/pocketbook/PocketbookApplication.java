3509f7b47a808840c7291dc8cf12800588e808c7
MAHMOUDI-FAROUK <118342402+MAHMOUDI-FAROUK@users.noreply.github.com>
2023-12-20 09:34:34 +0100

    modif

--- src/main/java/com/nefel/pocketbook/PocketbookApplication.java
+++ src/main/java/com/nefel/pocketbook/PocketbookApplication.java
@@ -1,13 +1,36 @@
 package com.nefel.pocketbook;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import net.javaguides.sms.entity.Expense;
+import net.javaguides.sms.repository.ExpenseRepos;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PocketbookApplication {
+public class PocketbookApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PocketbookApplication.class, args);
 	}
+	@Autowired
+	private ExpenseRepos expenseRepos;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		 Expense expense1 = new Expense("Farouk", "xxxxx", "54","xxxxxxxx");
+		 expenseRepos.save(expense1);
+		 
+		 Expense expense2 = new Expense("fathi", "yyyyy", "15","yyyyyyy");
+		 expenseRepos.save(expense2);
+		 
+		 Expense expense3 = new Expense("Ahlem", "zzzzzz", "64","zzzzzz");
+		 expenseRepos.save(expense3);
+		 
+		
+	}
+
 
 }

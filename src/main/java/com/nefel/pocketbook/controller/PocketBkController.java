df78e05e0640b14f26c6c19585e9b202fca3ff78
MAHMOUDI-FAROUK <118342402+MAHMOUDI-FAROUK@users.noreply.github.com>
2023-12-20 09:12:45 +0100

    new controller

--- src/main/java/com/nefel/pocketbook/controller/PocketBkController.java
+++ src/main/java/com/nefel/pocketbook/controller/PocketBkController.java
@@ -11,13 +11,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-//@RequestMapping("/expenses")
 @AllArgsConstructor
 public class PocketBkController {
 
     @Autowired
     ExpenseService service;
-
+// handler method to handle list expenses and return mode and view
     @GetMapping("/expenses")
     public String getExpenses(Model model){
         model.addAttribute("expenses", service.getAll());
@@ -26,28 +25,46 @@ public class PocketBkController {
 
     @GetMapping("/expenses/new")
     public String getExpense(Model model ){
+// create expense object to hold expense form data
         Expense expense= new Expense();
         model.addAttribute("expense", expense);
-
         return "create_expense";
     }
 
     @PostMapping("/expenses")
-    public String saveExpenses(@ModelAttribute("expense") Expense expense ){
+    public String saveExpense(@ModelAttribute("expense") Expense expense ){
         service.save(expense);
         return"redirect:/index";
     }
-
-    @PutMapping("/expenses/edit/{id}")
-    public Expense updateExpenses(@PathVariable("id") Integer id, Model model ){
-        model.addAttribute("expense", service.update(expense));
-        return "edit_expense";
-    }
-    @GetMapping("/expenses/edit/{id}")
+     @GetMapping("/expenses/edit/{id}")
 	public String editExpenseForm(@PathVariable Integer id, Model model) {
 		model.addAttribute("expense", servise.get(id));
 		return "edit_student";
 	}
+    @PostMapping("/expenses/{id}")
+	public String updateExpense(@PathVariable Integer id,
+			@ModelAttribute("expense") Expense expense,
+			Model model) {
+		
+		// get expense from database by id
+		Expense existingExpense = servise.get(id);
+		existingExpense.setId(id);
+		existingExpense.setName(Expense.getName());
+		existingExpense.setAmount(Expense.getAmount());
+		existingExpense.setVendor(Expense.getVendor());
+        existingExpense.setDescription(Expense.getDescription());
+		
+		// save updated expense object
+		servise.update(existingExpense);
+		return "redirect:/index";		
+	}
+
+   /* @PutMapping("/expenses/edit/{id}")
+    public Expense updateExpenses(@PathVariable("id") Integer id, Model model ){
+        model.addAttribute("expense", service.update(expense));
+        return "edit_expense";
+    }*/
+   
 
     @DeleteMapping("/expenses/delete/{id}")
     public void updateExpenses(@PathVariable("id") Integer id){
